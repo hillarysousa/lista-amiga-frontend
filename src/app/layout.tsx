c2fec07c9e5820e813bb910ad8df30bad0a13799
Hillary Sousa <hillaryhpf@gmail.com>
2025-04-30 00:49:25 -0300

    creating some structures and the login page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata, Viewport } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Fira_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const firaSans = Fira_Sans({
+  weight: ["400", "600", "700"],
   subsets: ["latin"],
 });
 
@@ -31,11 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${firaSans.className} antialiased`}>{children}</body>
     </html>
   );
 }
