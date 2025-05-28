b0611cdbb074fa264a609ff7943441082ae5eee2
Hillary Sousa <hillaryhpf@gmail.com>
2025-05-28 00:30:52 -0300

    fixing pages where suspense was needed

---
+++ src/app/(routes)/lists/content.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { useEffect } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useGetOwnLists } from "@/app/hooks/useGetOwnLists";
+import { useGetSharedLists } from "@/app/hooks/useGetSharedLists";
+import { useJoinList } from "@/app/hooks/useJoinList";
+import { List } from "@/app/types/list";
+import { EmptyListDashboard } from "@/app/components/emptyListsDashboard";
+import { ListCard } from "@/app/components/listCard";
+import { Loading } from "@/app/components/loadingFullScreen";
+
+export function ListsContent() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const shareToken = searchParams.get("share");
+
+  const {
+    data: ownLists,
+    isLoading: loadingOwnLists,
+    error: errorOwnLists,
+  } = useGetOwnLists();
+
+  const {
+    data: sharedLists,
+    isLoading: loadingSharedLists,
+    refetch: refetchSharedLists,
+    error: errorSharedLists,
+  } = useGetSharedLists();
+
+  const { mutate, isIdle, isPending } = useJoinList();
+
+  useEffect(() => {
+    if (shareToken) {
+      mutate(shareToken, {
+        onSuccess: () => {
+          const newParams = new URLSearchParams(searchParams.toString());
+          newParams.delete("share");
+          router.replace(`?${newParams.toString()}`, { scroll: false });
+          refetchSharedLists();
+        },
+      });
+    }
+  }, [mutate, refetchSharedLists, router, searchParams, shareToken]);
+
+  if (
+    loadingSharedLists ||
+    loadingOwnLists ||
+    (shareToken && (isIdle || isPending))
+  )
+    return <Loading />;
+
+  if (errorOwnLists || errorSharedLists) return <div>Erro!</div>;
+
+  return (
+    <div className="w-full flex flex-col h-full relative">
+      {ownLists && ownLists.length > 0 ? (
+        <>
+          {ownLists.map((list: List) => (
+            <ListCard
+              key={list.id}
+              listId={list.id}
+              name={list.name}
+              participants={list.participants}
+              createdDate={list.createdAt}
+              itemQuantity={list.items.length}
+            />
+          ))}
+        </>
+      ) : (
+        <EmptyListDashboard />
+      )}
+
+      <h1 className="text-darkBlue text-3xl font-bold mb-6 mt-9">
+        Listas compartilhadas
+      </h1>
+
+      {sharedLists && sharedLists.length > 0 ? (
+        <>
+          {sharedLists.map((list: List) => (
+            <ListCard
+              key={list.id}
+              listId={list.id}
+              name={list.name}
+              participants={list.participants}
+              createdDate={list.createdAt}
+              itemQuantity={list.items.length}
+            />
+          ))}
+        </>
+      ) : (
+        <EmptyListDashboard />
+      )}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,10 @@
-"use client";
-import { useEffect } from "react";
-import Image from "next/image";
-import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/navigation";
-import { UAParser } from "my-ua-parser";
-import Logo from "./assets/svg/logo_white.svg";
-import { useAuth } from "./providers/AuthProvider";
+import { Suspense } from "react";
+import { HomeContent } from "./content";
 
-export default function Home() {
-  const { login } = useAuth();
-  const searchParams = useSearchParams();
-  const shareToken = searchParams.get("share");
-
-  const userAgent = new UAParser();
-  const isDeviceNotMobile = userAgent.getDevice().type !== "mobile";
-  const router = useRouter();
-
-  useEffect(() => {
-    if (isDeviceNotMobile) {
-      return window.alert(
-        "Esse site pode ser visualizado apenas em dispositivos móveis."
-      );
-    }
-  });
-
-  useEffect(() => {
-    if (localStorage.getItem("accessToken")) {
-      router.push(shareToken ? `/lists?share=${shareToken}` : "/dashboard");
-    }
-  });
-
+export default function HomePage() {
   return (
-    <div className="grid justify-items-center min-h-screen h-screen pb-2 bg-lightYellow bg-[url(./assets/img/pattern.png)] content-between">
-      <div className="self-center mt-60">
-        <Image
-          src={Logo}
-          alt="Lista Amiga"
-          width={300}
-          className="mb-34"
-          priority
-        />
-        <a
-          onClick={login}
-          className="flex items-center border border-gray-600 rounded-lg shadow-sm bg-white hover:shadow-md transition px-4 h-12"
-        >
-          <div className="w-5 h-5 mr-3 flex items-center justify-center">
-            <Image
-              src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
-              alt="Google logo"
-              className="w-5 h-5"
-              width={10}
-              height={10}
-            />
-          </div>
-          <span className="text-darkText text-base font-normal">
-            Entrar com Google
-          </span>
-        </a>
-      </div>
-      <footer className="text-xs text-neutral-700 text-center self-end">
-        © 2025 Hillary Sousa — Visão, design e código
-      </footer>
-    </div>
+    <Suspense fallback={null}>
+      <HomeContent />
+    </Suspense>
   );
 }
